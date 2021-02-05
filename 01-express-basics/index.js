9b01254d8a4fb7c77c10bdc1fb65d966a39473a9
nhidayahj <hidayah.joha@gmail.com>
2021-02-05 04:11:23 +0000

    files 05 and 06 to be updated again

--- 01-express-basics/index.js
+++ 01-express-basics/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 
+
 let app = express()
 
 //add routes here
---
+++ 05-express-revision/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const hbs = require('hbs')
+const wax = require('wax-on')
+
+let app = express()
+
+app.get('/', (req,res) => {
+    res.send("Booking for Japanese Sakura Restaurant")
+
+})
+
+//one route to display the form
+app.get('/bookings/add', (req,res)=>{
+    res.render('add_booking.hbs')
+})
+
+// one route to process the form 
+app.post('/bookings/add', (req,res) => {
+    //display what is submitted
+    console.log(req.body);
+    res.send("data received")
+})
+
+
+
+
+// START SERVER
+app.listen(3000, () => {
+    console.log("Server has started")
+})
