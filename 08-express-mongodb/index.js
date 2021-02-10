6c216c36ff0e2bb063bd3403d4366f5976d50736
nhidayahj <hidayah.joha@gmail.com>
2021-02-10 08:19:08 +0000

    creating a new json file to be added into db

--- 08-express-mongodb/index.js
+++ 08-express-mongodb/index.js
@@ -9,14 +9,31 @@ const MongoUtil = require('./mongoUtil')
 // go to mongodb atlas - go to "connect" - change to connect to application - make sure driver is set to "Node.js" and copy the link 
 // put the url in the .env file 
 
+// create an instance of an Express app
 let app = express();
+
+// set the view engine
+app.set('view engine', 'hbs')
+
+// static folder
+app.use(express.static('public'))
+
+// setup wax-on 
+wax.on(hbs.handlebars)
+wax.setLayoutPath('./views/layouts')
+
+
+//enable forms
+app.use(express.urlencoded({
+    'extended':false
+}))
 
 async function main() {
     let db = await MongoUtil.connect(mongoUrl, 'tgc11_movie_app')
 
     // mongoDB is connected and live
+    app.get('/')
 
-    
 }
 
 main()
