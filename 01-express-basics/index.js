d91895e6214a0933baf272285ead3491a982db00
Hidayah <hidayah.joha@gmail.com>
2021-05-05 19:44:04 +0800

    test changes using vscode

--- 01-express-basics/index.js
+++ 01-express-basics/index.js
@@ -1,38 +1,36 @@
-const express = require('express')
+const express = require("express");
 
-
-let app = express()
+let app = express();
 
 //add routes here
-//Routes are mappings of a URL to a function 
+//Routes are mappings of a URL to a function
 // maps a URL to a JavaScript function
 // such that when the URL is accessed
 // on the server, the function will execute
 
 // this is the root route
-app.get('/', function(request,response) {
-    // we use the response object to send 
-    // the data back to the client (e.g. browser)
-    response.send("<h1>Hello from Express</h1>")
-})
+app.get("/", function (request, response) {
+  // we use the response object to send
+  // the data back to the client (e.g. browser)
+  response.send("<h1>Hello from Express</h1>");
+});
 
 // edit the end of the url with "/about-us" and the page will change
-app.get('/about-us', (req,res) => {
-    res.send("<h2>About Us Page</h2>")
-})
+app.get("/about-us", (req, res) => {
+  res.send("<h2>About Us Page</h2>");
+});
 
 // make another page "contact us"
-app.get('/contact-us', (req,res) => {
-    res.send("<h2>Contact Us Page</h2>")
-})
+app.get("/contact-us", (req, res) => {
+  res.send("<h2>Contact Us Page</h2>");
+});
 
 // include a parameter to our URL
-// at the end of the contact-us URL, include a fullname value 
-app.get('/contact-us/:fullname', (req,res) => {
-    let name = req.params.fullname;
-    res.send("You full name is " + name);
-})
-
+// at the end of the contact-us URL, include a fullname value
+app.get("/contact-us/:fullname", (req, res) => {
+  let name = req.params.fullname;
+  res.send("Your full name is " + name);
+});
 
 // include multiple parameters
 //app.get('/sum/:num1,:num2)', (req,res)
@@ -43,16 +41,16 @@ app.get('/contact-us/:fullname', (req,res) => {
 //     res.send("The sum of 2 numbers are: " + sum);
 // })
 
-app.get('/sum/:num1,:num2', (req,res)=>{
-    let num1 = parseInt(req.params.num1);
-    let num2 = parseInt(req.params.num2);
-    let sum =num1 + num2;
-    //res.send MUST be a string
-    res.send(sum.toString())
-})
+app.get("/sum/:num1,:num2", (req, res) => {
+  let num1 = parseInt(req.params.num1);
+  let num2 = parseInt(req.params.num2);
+  let sum = num1 + num2;
+  //res.send MUST be a string
+  res.send(sum.toString());
+});
 
-// ALL ROUTES must be added BEFORE the app.listen 
+// ALL ROUTES must be added BEFORE the app.listen
 
-app.listen(3000, ()=>{
-    console.log("Server has started")
-})
+app.listen(3000, () => {
+  console.log("Server has started");
+});
